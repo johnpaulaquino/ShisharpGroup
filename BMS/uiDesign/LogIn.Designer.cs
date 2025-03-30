ecdcadfb5e9ba0ff281d0f8210b765e4be2aae6b
Mark Larioza <mark_larioza@yahoo.com>
2025-03-30 10:48:32 +0800

    LogIn design

--- BMS/uiDesign/LogIn.Designer.cs
+++ BMS/uiDesign/LogIn.Designer.cs
@@ -61,7 +61,7 @@
             // 
             // kryptonLabel1
             // 
-            this.kryptonLabel1.Location = new System.Drawing.Point(256, 77);
+            this.kryptonLabel1.Location = new System.Drawing.Point(145, 126);
             this.kryptonLabel1.Name = "kryptonLabel1";
             this.kryptonLabel1.Size = new System.Drawing.Size(136, 26);
             this.kryptonLabel1.StateCommon.ShortText.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
